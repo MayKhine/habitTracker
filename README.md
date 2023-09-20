1cd84b84ba80441938710c5ccd6fcb54b04c9c47
May <maykhine.ada@gmail.com>
2023-09-20 12:43:08 -0400

    working on local stroage

--- README.md
+++ README.md
@@ -3,4 +3,26 @@
 ## React + TypeScript + Vite
 
 - project creation
+
   > pnpm create vite@latest habitTracker --template react-ts
+  > -luxon
+  > pnpm install --save-dev @types/luxon
+
+- MongoDb
+  - connect to mongo shell: mongosh
+  - choose db: use dbName (mydb)
+  - create a new collection: habitTracker
+    {
+    week: 1,
+    habitList: [
+    {
+    name: 'exercise',
+    achievement: [0, 1, 1, 1, 0, 0, 1]
+    },
+    {
+    name: 'study',
+    achievement: [1, 1, 1, 0, 0, 0, 0]
+    },
+    ]
+    }
+    > db.habitTracker.insert({ week: 1, habitList: [ { name: 'exercise', achievement: [0, 1, 1, 1, 0, 0, 1] }, { name: 'study', achievement: [1, 1, 1, 0, 0, 0, 0] }] })
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,70 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import "./App.css"
 import { Habit } from "./components/Habits"
-import { HabitsCanvas } from "./styledComponents/habitsCanvas"
+import { HabitsCanvas } from "./styledComponents/HabitsCanvas"
+import { DateTime } from "luxon"
 function App() {
+  const [habitTracker, setHabitTracker] = useState<JSON>(
+    JSON.parse(localStorage.getItem("habitTracker")) || {}
+  )
   const [habits, setHabits] = useState<Array<string>>([])
   const [habitText, setHabitText] = useState<string>("")
+
+  const now = DateTime.now()
+  const weekNumber = now.weekNumber
+  const weekDay = now.weekday
+
+  // console.log("HABIT TRACKER FROM LOCALSTORAGE: ", habitTracker)
+
   const addHabit = () => {
     console.log("Add Habit is clicked", habits)
+    //get the current week
     setHabits((habits) => [...habits, habitText])
     setHabitText("")
+    // go to current  week no and add it in the habits list
+    const curWeekHabitList =
+      habitTracker["weekNo"] == weekNumber ? habitTracker["habitList"] : []
+
+    const newHabit = {
+      name: habitText,
+      achievement: [0, 0, 0, 0, 0, 0, 0],
+    }
+
+    curWeekHabitList.push(newHabit)
+    console.log(
+      "updatedWeekHabitList , ",
+      curWeekHabitList,
+      habitTracker["habitList"]
+    )
+
+    // const newHabitTracker =
+    //   habitTracker["weekNo"] == weekNumber ? habitTracker : {}
+    // console.log("newHabitTracker: ", newHabitTracker)
+    // newHabitTracker.habitList = curWeekHabitList
+
+    //get cur week's habit list
+    // setHabitTracker((habitTracker) => {
+    //   curWeekHabitList
+    // })
   }
+
+  //set up the habits for the first time then commented it out
+  useEffect(() => {
+    console.log("Use Efffect is called")
+    // localStorage.setItem("habitTracker", JSON.stringify(habitTracker))
+
+    localStorage.setItem(
+      "habitTracker",
+      JSON.stringify({
+        weekNo: 38,
+        habitList: [
+          { name: "study", achievement: [1, 1, 1, 0, 0, 0, 0] },
+          { name: "exercise", achievement: [1, 1, 1, 0, 0, 0, 0] },
+        ],
+      })
+    )
+  }, [habitTracker])
+
   return (
     <>
       habit tracker
